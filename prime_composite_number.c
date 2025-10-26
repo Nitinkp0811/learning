ed0996697fbe80c7bc0a3c982762e736786f9382
Nitin Kumar Patel <nitin8112006@gmail.com>
2025-10-26 02:54:11 -0700

    Update prime_composite_number.c

--- prime_composite_number.c
+++ prime_composite_number.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 int main() {
-    int n, prime = 0;
+    int n, not_prime = 0;
     printf("Enter the number: ");
     scanf("%d", &n);
 
     for (int i = 2; i < n; i++)
     {
-        if (n % i == 0){
-            prime = 1;
+        if (n % i == 0 && n != 2){
+            not_prime = 1;
+            break;
         }
     }
 
-    if (prime){
+    if (not_prime){
         printf("%d is a composite number\n", n);
     }
     else{
@@ -21,4 +22,5 @@ int main() {
     }
     
     return 0;
+
 }
